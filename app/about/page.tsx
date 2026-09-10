aed162d3f53b5aa585937dd683ae04928993ff07
oedesign <oluwaseyielujoba15@gmail.com>
2026-09-10 12:48:44 +0100

    Week01 completed

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <section>
+      <h1 className="text-4xl font-bold">About Me</h1>
+
+      <p className="mt-4">
+        I am a web development student learning how to build modern
+        applications with Next.js.
+      </p>
+    </section>
+  );
+}
